1c927382c0d03c91bcd2c5a2d2b300e78f516667
Federico Urquiza <federicourquizaranieri@gmail.com>
2026-05-10 19:28:57 -0300

    integrating disputes
    Added the disputes various pages.
    This pages were implemented by using the transactions

--- payments_app/app/dashboard/disputes/page.tsx
+++ payments_app/app/dashboard/disputes/page.tsx
@@ -1,7 +1,45 @@
-export default function DisputesPage() {
+import TransactionsList from "../../../components/ui/OperationList";
+
+const disputes = [
+  {
+    id: "1",
+    title: "Transaccion 58266",
+    subtitle: "Cobro doble • En revision",
+    amount: "$45000.00",
+  },
+  {
+    id: "2",
+    title: "Transaccion 34596",
+    subtitle: "Fallo pago • En revision",
+    amount: "$10,200.00",
+  },
+  {
+    id: "3",
+    title: "Transaccion 34589",
+    subtitle: "Pago resuelto • Cerrado",
+    amount: "$89.99",
+  },
+];
+
+export default function TransactionsPage() {
   return (
-    <div>
-      <h1>Showing all disputes</h1>
-    </div>
-  )
+    <main className="min-h-screen bg-zinc-100 p-8">
+
+      <div className="mb-8">
+        <h1 className="text-5xl font-bold text-zinc-800">
+          Historial de disputas
+        </h1>
+
+        <p className="mt-2 text-zinc-500">
+          Historial detallado de las disputas accionadas por tu cuenta.
+        </p>
+      </div>
+
+      <TransactionsList
+        title="Disputas"
+        items={disputes}
+      />
+
+    </main>
+  );
 }
